06ca99930720b17b05c4dfb8d6323e6416d25556
4ydak2008@gmail.com <4ydak2008@gmail.com>
2017-03-20 00:40:48 +0300

    deleting files

--- Final_Maksimovich/src/by/epam/totalizator/controller/command/Command.java
+++
@@ -1,8 +0,0 @@
-package by.epam.totalizator.controller.command;
-
-import javax.servlet.http.HttpServletRequest;
-
-public interface Command {
-
-	public String execute(HttpServletRequest request);
-}
