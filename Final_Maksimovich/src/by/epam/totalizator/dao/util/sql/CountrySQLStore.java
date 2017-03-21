4f7e725e858ace6262d4eb2f2f79dc8623e31334
4ydak2008@gmail.com <4ydak2008@gmail.com>
2017-03-21 23:28:01 +0300

    deleting

--- Final_Maksimovich/src/by/epam/totalizator/dao/util/sql/CountrySQLStore.java
+++
@@ -1,7 +0,0 @@
-package by.epam.totalizator.dao.util.sql;
-
-public class CountrySQLStore {
-
-	public static final String GET_COUNTRY_LIST = 
-			"select country.id, country.name from totalizator.country;";
-}
--- Final_Maksimovich/src/by/epam/totalizator/dao/util/sql/TournamentSQLStore.java
+++
@@ -1,7 +0,0 @@
-package by.epam.totalizator.dao.util.sql;
-
-public class TournamentSQLStore {
-
-	public static final String GET_TOURNAMENT_LIST_BY_ID_SPORT =
-			"SELECT tournament.id, tournament.name FROM totalizator.tournament where tournament.id_sport= ?;";
-}
